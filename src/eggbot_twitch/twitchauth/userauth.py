661d5b90304b52a095cb48c8b47373b77b4edb05
Preocts <preocts@preocts.com>
2025-09-02 21:12:21 -0400

    Load UserAuth from a saved file object (json)

--- src/eggbot_twitch/twitchauth/userauth.py
+++ src/eggbot_twitch/twitchauth/userauth.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 
 import dataclasses
+import json
 import time
-from typing import Any
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from _typeshed import SupportsRead
 
 
 @dataclasses.dataclass(frozen=True, slots=True)
@@ -19,7 +25,11 @@ class UserAuth:
     @classmethod
     def parse_response(cls, response: dict[str, Any]) -> UserAuth:
         """Build from authorization response from TwitchTV."""
-        expires_at = int(response["expires_in"] + time.time())
+        if "expires_at" in response:
+            expires_at = response["expires_at"]
+        else:
+            expires_at = int(response["expires_in"] + time.time())
+
         return cls(
             access_token=response["access_token"],
             expires_in=response["expires_in"],
@@ -28,3 +38,8 @@ class UserAuth:
             scope=tuple(response["scope"]),
             token_type=response["token_type"],
         )
+
+    @classmethod
+    def load(cls, fp: SupportsRead[bytes]) -> UserAuth:
+        """Load UserAuth from a file. Must be in JSON format."""
+        return cls.parse_response(json.load(fp))
---
+++ tests/twitchauth/userauth_test.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import json
+import tempfile
+
+from eggbot_twitch.twitchauth import UserAuth
+
+MOCK_USER_AUTH = {
+    "access_token": "mock_access_token",
+    "expires_in": 15701,
+    "expires_at": 1756875356,
+    "refresh_token": "mock_refresh_token",
+    "scope": ["user:read:chat", "user:read:email"],
+    "token_type": "bearer",
+}
+
+json.load
+
+
+def test_load_valid_file() -> None:
+    with tempfile.TemporaryFile() as authfile:
+        authfile.write(json.dumps(MOCK_USER_AUTH).encode())
+        authfile.read()
+        authfile.seek(0)
+
+        userauth = UserAuth.load(authfile)
+
+    assert userauth.access_token == "mock_access_token"
+    assert userauth.expires_in == 15701
+    assert userauth.expires_at == 1756875356
+    assert userauth.refresh_token == "mock_refresh_token"
+    assert userauth.scope == ("user:read:chat", "user:read:email")
+    assert userauth.token_type == "bearer"
