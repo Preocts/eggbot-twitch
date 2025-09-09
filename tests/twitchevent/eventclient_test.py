f1b2788b681cb5d46f1098b23afbd2cfb9c1805c
Preocts <preocts@preocts.com>
2025-09-08 23:36:35 -0400

    Frame a mock websocket server for tests

---
+++ tests/twitchevent/eventclient_test.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import threading
+import time
+from functools import partial
+
+from websockets.sync.server import ServerConnection
+from websockets.sync.server import serve
+
+
+class MockEventServer(threading.Thread):
+
+    def __init__(self, host: str, port: int) -> None:
+        super().__init__()
+        self.host = host
+        self.port = port
+        self.timeout_at = time.time() + 2
+
+    def run(self) -> None:
+        """Run the websocket server forever."""
+        handler = partial(self.message_handler, self.timeout_at)
+        with serve(handler, self.host, self.port) as server:
+            server.serve_forever()
+
+    @staticmethod
+    def message_handler(timeout_at: float, websocket: ServerConnection) -> None:
+        while time.time() < timeout_at:
+            message = websocket.recv()
+
+            print(f"Recieved: {message=}")
