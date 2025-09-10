b5b9cefd269dda0f431189be60ad8336acb08b1f
Preocts <preocts@preocts.com>
2025-09-10 01:23:43 -0400

    Include new start_session_thread function

--- src/eggbot_twitch/twitchevent/__init__.py
+++ src/eggbot_twitch/twitchevent/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from ._eventclient import get_session_id
+from ._eventclient import start_session_thread
 
 __all__ = [
     "get_session_id",
+    "start_session_thread",
 ]
